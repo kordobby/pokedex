f9825a59957cdb8265b2f31461289f4b6b405a96
kordobby <poordobby@naver.com>
2022-06-01 20:37:34 +0900

    move data load feature

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,25 +2,37 @@ import styled from 'styled-components';
 import PostBox from '../components/PostBox';
 
 import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { loadPokeFB } from '../redux/modules/pokeReducer';
-
 function Home(  ) {
+  // 카드에서 활용할 데이터 리스트 받아오기
+  const myPokeList = useSelector((state) => state.pokeReducer.list);
 
   const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(loadPokeFB());
-  })
-  // console.log(myPokeList);
+
+    useEffect(() => {
+      dispatch(loadPokeFB());
+    }, [dispatch]);
+
 
 return (
   <>
     <MainGrid> { /* grid father */ }
-      <PostBox/> { /* grid sons*/ }
+      { myPokeList.map((value, index) => {
+        return <PostBox 
+        key = {value.id} 
+        pokeIdx = {value.pokeIdx} 
+        pokeName = {value.pokeName} 
+        pokeType = {value.pokeType} 
+        pokeStory = {value.pokeStory} 
+        pokeStats = {value.pokeStats}
+        idx = {index}
+        id = {value.id}/>;
+      })} { /* grid sons*/ }
     </MainGrid>
     <BtnPosition>
-      <Link to = "/post">
+      <Link to = "/post" >
         <LinkBtn>+</LinkBtn>
       </Link>
     </BtnPosition>
@@ -53,10 +65,14 @@ const LinkBtn = styled.div`
   width : 50px;
   height: 50px;
   border-radius: 50px;
-  background-color: var(--blue);
+  background-color: #dc0a2d;
 
   display : flex;
   justify-content: center;
   align-items: center;
+
+  color : white;
+  font-size : 25px;
+  font-weight : 800;
 `
 export default Home;
