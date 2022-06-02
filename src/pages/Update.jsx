b32f1841c5e2e336892302983cc597b0faab2f27
kordobby <poordobby@naver.com>
2022-06-02 23:40:44 +0900

    update 공부내용 주석 추가, 오류 수정

--- src/pages/Update.jsx
+++ src/pages/Update.jsx
@@ -9,51 +9,73 @@ function Update() {
 
     // parameter 받아오기 : id, idx
     const { id, idx } = useParams();
+    // console.log(id + "+" + idx);
+    /* Store data 가져오기 */
+    // 나는 여기서 기존 저장된 데이터를 input 창에 띄워서 수정하기 편하게 해줄거라서!
 
-    // list destructuring, data 
-    const { list } =  useSelector((state) => state.pokeReducer);
-    // const pokeMemo = list;
-    // console.log(pokeMemo); => 확인완료
+    const pokeMemo = useSelector((state) => state.pokeReducer.list); // 내 Store 에 있는 모든 data list 를 가져오게 됨
+    const pokeData = pokeMemo[idx]; // 내 Store 의 모든 데이터 리스트 중 해당 페이지에 있는 데이터만 가져옴
+    // console.log(pokeData); => 콘솔은 신이다....
+    // console.log(pokeData.pokeIdx); 
 
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
-    // useRef
+    /* Input value 받아오기 위해 DOM 접근, useRef */
     const newPokeName = useRef();
     const newPokeIdx = useRef();
     const newPokeStats = useRef();
     const newPokeType = useRef();
     const newPokeStory = useRef();
 
-    // update event
+    /* Input value load */
+    useEffect( () => {
+      newPokeIdx.current.value = pokeData?.pokeIdx;
+      newPokeName.current.value = pokeData?.pokeName;
+      newPokeType.current.value = pokeData?.pokeType;
+      newPokeStory.current.value = pokeData?.pokeStory;
+      newPokeStats.current.value = pokeData?.pokeStats;
+    });
+
+
+    /* [ UPDATE :: THUNK FUNCTION 참고 ]
+    export const updatePokeFB = (payload, index) => async (dispatch) => {
+      const docRef = doc(db, "pokemon", payload.id);
+      await updateDoc(docRef, {
+        pokeIdx : payload.pokeIdx,
+        pokeName : payload.pokeName,
+        pokeStats : payload.pokeStats,
+        pokeStory : payload.pokeStory,
+        pokeType : payload.pokeType
+      });
+      dispatch(updatePoke({payload, index}))
+    };
+    */
+
     const handlePokeUpdate = () => {
-      if (
-        newPokeName.current.value !== "" &&
-        newPokeIdx.current.value !== "" &&
-        newPokeStats.current.value !== ""
-        ) {
+      // required 를 먹여놨는데, 굳이 아래 주석을 입힌 코드를 돌릴 이유가 있을까?
+      // if (
+      //   newPokeName.current.value !== "" &&
+      //   newPokeIdx.current.value !== "" &&
+      //   newPokeStats.current.value !== "" &&
+      //   newPokeStory.current.value !== "" &&
+      //   newPokeType.current.value !== ""
+      //   ) {
         dispatch(updatePokeFB({
           id : id,
           pokeIdx: newPokeIdx.current.value,
           pokeName : newPokeName.current.value,
           pokeStats: newPokeStats.current.value,
           pokeStory :newPokeStory.current.value,
-          pokeType :newPokeType.current.value}, idx )          
+          pokeType :newPokeType.current.value}, idx )  // 클릭한 아이의 idx 값을 보내서 DB에서 찾아서 수정!
         );
         alert ("도감 수정 완료!");
         navigate("/");
-      }else {
-        alert ("다시 작성해주세요!");
-      }
+      // }else {
+      //   alert ("다시 작성해주세요!");
+      // }
     };
 
-    // useEffect( () => {
-    //   newPokeIdx.current.value = pokeMemo?.pokeIdx;
-    //   newPokeName.current.value = pokeMemo?.pokeName;
-    //   newPokeType.current.value = pokeMemo?.pokeType;
-    //   newPokeStory.current.value = pokeMemo?.pokeStory;
-    //   newPokeStats.current.value = pokeMemo?.pokeStats;
-    // });
     
 return (
     <>
